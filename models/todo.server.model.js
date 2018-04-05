e9b99df5a1ffa858935a6980e3526947dbc728af
David Kironde <david.kironde@andela.com>
2018-04-05 12:09:05 +0300

    add schemas

--- models/todo.server.model.js
+++ models/todo.server.model.js
@@ -1,11 +1,31 @@
 const mongoose = require ("mongoose");
 
-const Schema = mongoose.Schema({
-  createdAt:{
+const Schema = mongoose.Schema;
+
+const TodoItemsSchema = new Schema ({
+  text: String,
+  CreatedAt: {
     type: Date,
     default: Date.now
   },
-  fullName: String,
+  UpdatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  status: {type: Boolean, default: false}
+});
+
+const TodoListSchema = new Schema ({
+  text: String,
+  CreatedAt:{
+    type: Date,
+    default: Date.now()
+  },
+  UpdatedAt: {
+    type: Date,
+    default: Date.now()
+  },
+  items: [TodoItemsSchema],
   todoText: String
 });
 
