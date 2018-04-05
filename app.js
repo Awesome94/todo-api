b618a67ed9754197accf6228c1825bfdef28028c
David Kironde <david.kironde@andela.com>
2018-04-05 11:13:44 +0300

    add models and error handlers

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import path from "path";
-import bodyParser from 'body-parser';
-import logger from "morgan";
-import mongoose from "mongoose";
-import bb from "express-busboy";
+const express  = require('express');
+const path = require("path");
+const bodyParser  = require('body-parser');
+const logger = require("morgan");
+const mongoose = require ("mongoose");
 
-const app = express
+const app = express()
+
+mongoose.connect("mongodb://localhost:27017/todolist")
 
 app.use(function (req, res, next) {
   res.header("Allow-Control-Allow-Origin", "*");
@@ -13,8 +14,42 @@ app.use(function (req, res, next) {
   next();
 })
 
-PORT = process.env.PORT
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.get('/v1', (req, res) => {
+return res.send("api is in action")
+})
+app.post('/v1/signin', (req, res) => {
+return(res.send("you are trying to access v1/signin"))
+})
+app.post('/v1/register')
+app.get('/v1/profile/:id')
+app.post('/v1/todo')
+app.get('/v1/todo/:id')
+app.put('/v1/todo/:id')
+app.delete('/v1/todo')
+app.delete('/v1/todo/:id')
+
+app.use((err, req, res, next) => {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>{
  console.log(`app is listening on port ${PORT}`)
 })
---
+++ models/todo.server.model.js
@@ -0,0 +1,12 @@
+const mongoose = require ("mongoose");
+
+const Schema = mongoose.Schema({
+  createdAt:{
+    type: Date,
+    default: Date.now
+  },
+  fullName: String,
+  todoText: String
+});
+
+export default mongoose.model('Todo', Schema);
