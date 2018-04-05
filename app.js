c21188984484be3f484092273f092b0e56e04dbe
David Kironde <david.kironde@andela.com>
2018-04-05 18:46:50 +0300

    separating concerns

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const TodoList = require("./models/models").TodoList;
 const Login = require("./models/models").Login;
-const User = require("./models/models").User;
+const User = require("./models/userSchema")
+const bcrypt = require('bcrypt')
 
 
 const passport = require('passport');
 var expressSession = require("express-session");
-
-
 
 const app = express();
 
@@ -80,12 +79,58 @@ app.post('/v1/signin', (req, res) => {
 })
 
 app.post('/v1/register', (req, res) => {
-  passport.use('register', new localStrategy({
-    passReqToCallback: true
-  },
-  (req, username, password, done)=>{
+  const { firstName, lastName, password } = req.body;
+  let { email } = req.body
+  if (!firstName){
+    return res.json({
+      success: false,
+      message: 'Error First Name cannot be blank'
+    });
   }
-))
+  if(!email){
+    return res.json({
+      success: false,
+      message: 'Error email cannot be blank'
+    });
+  }
+  if(!password){
+    return res.send({
+      success: false,
+      message: 'Error password cannot be blank'
+    });
+  }
+email = email.toLowerCase();
+
+User.find({
+  email
+}, (err, previousUsers)=>{
+  if(err){
+    return res.send('Error: server error');
+  }else if (previousUsers.lenght > 0) {
+    return  res.send({
+      success: false,
+      message:'Error account already exists'
+    });
+  }
+
+  const newUser = new User();
+
+  newUser.email = email;
+  newUser.firstName = firstName;
+  newUser.lastName = lastName;
+  newUser.password = newUser.generateHash(password);
+
+  newUser.save((err, user) => {
+    if(err) {
+      console.log(err)
+     return res.send({
+        success: false,
+        message: 'Error Server Error'
+      });
+    }
+    return res.json("signed up")
+    });
+  })
 });
 
 app.get('/v1/profile/:id')
@@ -95,20 +140,20 @@ app.put('/v1/todo/:id')
 app.delete('/v1/todo')
 app.delete('/v1/todo/:id')
 
-app.use((err, req, res, next) => {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
+// app.use((err, req, res, next) => {
+//   var err = new Error("Not Found");
+//   err.status = 404;
+//   next(err);
+// });
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    error: {
-      message: err.message
-    }
-  });
-});
+// app.use((err, req, res, next) => {
+//   res.status(err.status || 500);
+//   res.json({
+//     error: {
+//       message: err.message
+//     }
+//   });
+// });
 
 const PORT = process.env.PORT || 4000;
 
--- models/models.js
+++ models/models.js
@@ -2,20 +2,6 @@ const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-firstName: {
-  type: String, required: true, default:'' },
-lastName: {
-  type:String, default:''},
-email: {
-  type: String, required: true, unique:true, default:''},
-password: {
-  type: String, required: true, default:''},
-isDeleted: {
-  type: Boolean, default:false
-}
-});
-
 const LoginSchema = new Schema({
 username: {type: String, required: true, index: { unique: true} },
 password: {type: String, required: true, }
@@ -51,16 +37,5 @@ var TodoList = mongoose.model("TodoList", TodoListSchema)
 
 var Login = mongoose.model("Login", LoginSchema)
 
-var User = mongoose.model("User", UserSchema)
-
-UserSchema.methods.generateHash = function(pasword) {
-  return bcrypt.hashSync(password, bcrypt.genSaltsnyc(8), null);
-}
-
-UserSchema.methods.validPassword = function (password) {
-  return bcrypt.compareSync(password, this.password);
-};
-
 module.exports.TodoList = TodoList;
 module.exports.Login = Login;
-module.exports.User = User;
